9631545f80989a6f8dbf677da94d7ee5f8763bbe
Arseniks <senya.korotkov@gmail.com>
2023-05-04 22:54:00 +0300

    Small refactoring

--- friends_service/urls.py
+++ friends_service/urls.py
@@ -12,23 +12,28 @@ urlpatterns = [
         'admin/',
         admin.site.urls,
     ),
-    path(
-        'api/schema/',
-        SpectacularAPIView.as_view(),
-        name='schema',
-    ),
-    path(
-        'api/schema/swagger-ui/',
-        SpectacularSwaggerView.as_view(url_name='schema'),
-        name='swagger-ui',
-    ),
 ]
 
 
 if settings.DEBUG:
     import debug_toolbar
 
-    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
+    urlpatterns += (
+        path(
+            '__debug__/',
+            include(debug_toolbar.urls)
+        ),
+        path(
+            'api/schema/',
+            SpectacularAPIView.as_view(),
+            name='schema',
+        ),
+        path(
+            'api/schema/swagger-ui/',
+            SpectacularSwaggerView.as_view(url_name='schema'),
+            name='swagger-ui',
+        ),
+    )
 
     if hasattr(settings, 'MEDIA_ROOT'):
         urlpatterns += static(
