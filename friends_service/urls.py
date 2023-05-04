cd14d2c50c8f99cdb7a79f7da0319e34da26fd8a
Arseniks <senya.korotkov@gmail.com>
2023-05-04 22:39:17 +0300

    Adding OpenAPI supporting

--- friends_service/urls.py
+++ friends_service/urls.py
@@ -7,11 +7,21 @@ from django.urls import path
 from drf_spectacular.views import SpectacularAPIView
 from drf_spectacular.views import SpectacularSwaggerView
 
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path(
+        'admin/',
+        admin.site.urls,
+    ),
+    path(
+        'api/schema/',
+        SpectacularAPIView.as_view(),
+        name='schema',
+    ),
+    path(
+        'api/schema/swagger-ui/',
+        SpectacularSwaggerView.as_view(url_name='schema'),
+        name='swagger-ui',
+    ),
 ]
 
 
