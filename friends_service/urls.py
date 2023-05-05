00967dd10faeb21aa793f86785d3242f07a92389
Arseniks <senya.korotkov@gmail.com>
2023-05-06 00:46:30 +0300

    Small refactoring

--- friends_service/urls.py
+++ friends_service/urls.py
@@ -1,9 +1,10 @@
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
 from django.contrib.auth import urls
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import include
+from django.urls import path
 
 import home.urls
 import users.urls
