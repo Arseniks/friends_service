5297c3827382903ae34608cdebcabbfcef3c77df
Arseniks <senya.korotkov@gmail.com>
2023-05-10 21:53:35 +0300

    Refactoring to linting

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,3 @@
-import django.contrib.auth.views
 from django.urls import path
 
 import users.views
@@ -6,32 +5,32 @@ import users.views
 app_name = 'users'
 
 urlpatterns = [
-    django.urls.path(
+    path(
         'api/v1/user_list/',
         users.views.UserListAPIView.as_view(),
         name='user_list',
     ),
-    django.urls.path(
+    path(
         'api/v1/pending_list/',
         users.views.PendingListAPIView.as_view({'get': 'list'}),
         name='pending_list',
     ),
-    django.urls.path(
+    path(
         'api/v1/pending_list/delete/<int:pk>/',
         users.views.PendingListAPIView.as_view({'delete': 'destroy'}),
         name='pending_list_delete',
     ),
-    django.urls.path(
+    path(
         'api/v1/pending_list/accept/<int:pk>/',
         users.views.PendingListAPIView.as_view({'put': 'update'}),
         name='pending_list_accept',
     ),
-    django.urls.path(
+    path(
         'api/v1/friend_list/',
         users.views.FriendsListAPIView.as_view({'get': 'list'}),
         name='friend_list',
     ),
-    django.urls.path(
+    path(
         'api/v1/friend_list/delete/<int:pk>/',
         users.views.FriendsListAPIView.as_view({'delete': 'destroy'}),
         name='friend_list_delete',
